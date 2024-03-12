7e8baf3749917ddaeea1ce3b3b08a3beee30a6c4
joehecn <leanbrown@live.cn>
2024-03-12 14:03:19 +0800

    Expose validateSchema to cedar-wasm

---
+++ cedar-wasm/src/schema_validator.rs
@@ -0,0 +1,23 @@
+use std::str::FromStr;
+
+use cedar_policy::Schema;
+
+use tsify::Tsify;
+use wasm_bindgen::prelude::*;
+
+#[derive(Tsify, Debug, Serialize, Deserialize)]
+#[tsify(into_wasm_abi, from_wasm_abi)]
+pub enum SchemaValidationResult {
+    Success { ok: Boolean },
+    Error { errors: Vec<String> },
+}
+
+#[wasm_bindgen(js_name = "validateSchema")]
+pub fn wasm_validate_schema(input: &str) -> SchemaValidationResult {
+    match Schema::from_str(input) {
+        Ok(_) => SchemaValidationResult::Success { ok: true },
+        Err(e) => SchemaValidationResult::Error {
+            errors: vec![e.to_string()],
+        },
+    }
+}
